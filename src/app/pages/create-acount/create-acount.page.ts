288e556683d78fc8109ad8dad33c553a93ba3c0b
Emiliano <2123100595@soy.utj.edu.mx>
2025-03-07 08:01:24 -0600

    Primer commit

---
+++ src/app/pages/create-acount/create-acount.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-create-acount',
+  templateUrl: './create-acount.page.html',
+  styleUrls: ['./create-acount.page.scss'],
+})
+export class CreateAcountPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
